b4534b30614ce59dd51bed5fbb14b9517b0a07b9
DwiFatimaA <3103120072@student.smktelkom-pwt.sch.id>
2023-03-11 18:31:22 +0700

    first commit

---
+++ resources/views/admin-lte/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="row justify-content-center">
+      <strong>Dwi Fatima Azzahra | XII RPL 3 | SMK Telkom Purwokerto</strong>
+    </div>
+</footer>
---
+++ resources/views/admin-lte/preloader.blade.php
@@ -0,0 +1,3 @@
+ <div class="preloader flex-column justify-content-center align-items-center" style="background: linear-gradient(to right, #C3ACD0, #674188);">
+    <img class="animation__shake" src="/adminlte/dist/img/zarbook.png" alt="ZarbookLogo" height="100" width="100">
+  </div>
